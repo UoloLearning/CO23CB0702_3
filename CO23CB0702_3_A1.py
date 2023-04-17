cf5d40ffdc1d015ebf4b5bbcd77605e60493cece
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-17 17:59:57 +0530

    Update CO23CB0702_3_A1.py

--- CO23CB0702_3_A1.py
+++ CO23CB0702_3_A1.py
@@ -1,8 +1,4 @@
-# Ask the user to input a number
-num = int(input("Enter a number: "))
-
-# Create a nested loop to generate the multiplication table
-for i in range(1, 11):
-    for j in range(1, num+1):
-        print(j, "x", i, "=", i*j)
-    print("\n")
+for i in range(1, 6):
+    for j in range(i):
+        print('*', end='')
+    print()
