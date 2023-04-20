39e98c6b668bca34758b088b7d0b0b60bcce7aca
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-20 11:58:47 +0530

    Update CO23CB0702_3_AA1.py

--- CO23CB0702_3_AA1.py
+++ CO23CB0702_3_AA1.py
@@ -1,6 +1,6 @@
-numbers = [23, 56, 101, 78, 90, 120, 67]
+for i in range(10):
+    num = int(input("Enter a number: "))
 
-for num in numbers:
     if num > 100:
         print("Number greater than 100 entered. Exiting loop.")
         break
